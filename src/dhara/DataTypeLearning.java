c3b9c728bffb12a22a7035bd4cc18f30b32707d2
Rohan <rohandhara2002@gmail.com>
2024-02-27 16:53:53 +0530

    data types here

--- src/dhara/DataTypeLearning.java
+++ src/dhara/DataTypeLearning.java
@@ -1,7 +1,27 @@
 package dhara;
+/*
+    There are two categories of datatype in java
+    1.Primitive 2. Non-Primitive
+    1.Primitive --->> int(byte,short,int,long),double(float,double),char,boolean etc .
+    int has default value 0 and double has 0.0
+    default value of boolean in java is false
+    2.Non-Primitive --->> String,Array,Object,Interaface etc.
 
+ */
 public class DataTypeLearning{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        byte bt = 127;  // 1 byte
+        short sh = 32767; // 2 byte
+        int i = 2147483647; // 4 byte
+        long l = 9223372036854775807l; // 8 byte
+
+        float ft = 12.72F;
+        double d = 78.1245;
+
+        char c= 'a';
+
+        boolean bl = true;
+
     }
 }
