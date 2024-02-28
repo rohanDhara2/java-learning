1757302a81f33eeffc7ef7aef5fcc902e59917cc
Rohan <rohandhara2002@gmail.com>
2024-02-28 16:36:09 +0530

    variables

--- src/dhara/Variables.java
+++ src/dhara/Variables.java
@@ -2,6 +2,17 @@ package dhara;
 
 public class Variables {
     public static void main(String[] args) {
-        System.out.println(12);
+       int num = 10;
+       int _num2 = 20;
+       int result = num+_num2;
+   //    int class = 12; // this is not allowed as class is a reserved keyword
+       System.out.println(result);
+       /*
+            num,num2,result all are variables . Basically variable is used to hold data.
+            We can give any name to a variable but following some default patterns
+            Variables can only start with any alphabets or _ we cannot use numbers at the starting of a variable but
+            in middle or end or anywhere in between. Also, we cannot use any reserved keyword as a variable name.
+
+        */
     }
 }
