9095fa614723dcab9d478a505bdc9e3ddda16083
Rohan <rohandhara2002@gmail.com>
2024-03-06 15:52:27 +0530

    first string program

--- src/dhara/StringOne.java
+++ src/dhara/StringOne.java
@@ -9,7 +9,14 @@ package dhara;
 public class StringOne {
     public static void main(String[] args) {
         String myName = new String("Rohan"); //we can pass value to constructor for set the myName value like this.
+        String myHouse = "Pinkvilla"; // it will also create an object behind the scene and point to pinkvilla.
         //concatenate
-        System.out.println("my name is "+myName);
+        System.out.println("my name is "+myName+" ");
+        //methods
+        System.out.println(myName.equals(myHouse)); // returns a boolen value after checking the strings are equal or not.
+        System.out.println(myName.concat(" Dhara"));//concat with given string
+        System.out.println(myName.charAt(2));//returns the character at specified index
+        System.out.println(myName.hashCode());//generate hash code of the string
+        System.out.println(myName.toCharArray()); //for making the string a character array
     }
 }
