efbcf37ddf064519e56bdc068b729cebb54b5c0a
Rohan <rohandhara2002@gmail.com>
2024-03-07 18:41:13 +0530

    encapsulation and getter setter

--- src/dhara/Encapsulation.java
+++ src/dhara/Encapsulation.java
@@ -10,17 +10,20 @@ class Human{
     private String name;
     private int age;
 
-    public void setAge(int a){
-        age=a;
+    public String getName() {
+        return name;
     }
-    public int getAge(){
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
         return age;
     }
-    public void setName(String n){
-        name = n;
-    }
-    public String getName(){
-        return name;
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
 public class Encapsulation {
